f3af733fec40d8a56f55c20289557b8f5fc6d32f
DESKTOP-9N1JJ0G\User <headisnotsmall>
2020-10-08 13:04:55 +0800

    update

--- crawler/crawler_imdb.py
+++ crawler/crawler_imdb.py
@@ -5,16 +5,17 @@ import os
 
 page = 1
 
-while True:
+while page < 200:
     try:
         url = "https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&count=100&start=" + str(page) + "&ref_=adv_nxt"
         p = requests.get(url).text
-        html = bs(p)
-        main_content = html.find_all("div", class_="bbs-screen bbs-content") # movie pack
-        for content in main_content:
-            name = content.find_all("a")
-            score = content.find_all("strong")
-            print(name.text, score.text)
-        page += 100
+    #     html = p
+    #     main_content = html.find_all('div', id_="lister-item-content") # movie pack
+    #     for content in main_content:
+    #         name = content.find_all("a")
+    #         score = content.find_all("strong")
+    #         print(name.text, score.text)
+    #     page += 100
     except:
         pass
+print(p)
