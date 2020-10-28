6c84e27a5ce8af9744604e1ef7c2d72b28574612
Niels Liao <headinsotsmall@gmail.com>
2020-10-28 20:41:32 +0800

    update django

--- archive/concat.sql
+++ archive/concat.sql
@@ -1,14 +1,8 @@
-SELECT * FROM public.movies 
-WHERE YEAR > 2015 AND VOTES > 3000
-ORDER BY avg_vote DESC
-LIMIT 100;
+--有中文的電影評分排序
 
-SELECT * FROM public.movies
-WHERE original_title like '%Jurassic%'``
-
-SELECT original_title, tw_title, director, name, avg_vote FROM public.movies
+SELECT original_title, tw_title, year, director, name, avg_vote FROM public.movies
 JOIN casts on movies.imdb_title_id = casts.imdb_title_id
 JOIN names on casts.imdb_name_id = names.imdb_name_id
 LEFT JOIN tw_movie on movies.imdb_title_id = tw_movie.imdb_title_id
-WHERE votes > 5000 and ordering = 1
-LIMIT 100
+WHERE votes > 5000 and ordering = 1 and tw_title is not null
+ORDER BY avg_vote DESC
